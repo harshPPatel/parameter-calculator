517ff36465e4b62c98b71725b1f9f87276fb0934
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-08 13:17:30 -0600

    main method

--- parameterCalculator.c
+++ parameterCalculator.c
@@ -11,8 +11,50 @@
  * Description  : Calls every method and run them
  * Method Type	: void 
  */
-int main() {
-	/* Code */
-	getch();
-	return 0;
+void main() {
+	/* Variables */
+	int userChoice;
+	
+	/* While Loop to Continue to take input after getting area of one shape. */
+	while(true) {
+		
+		/* Printing instruction */
+		printf("\nChoose Your Code : \n  1 - Square\n  2 - Circle\n  3 - Rectangle\n  4 - Cube\n  5 - Cone\n  6 - Sphere\n  7 - Cylinder\n  8 - Exit\n");
+		
+		/* Taking User Input */
+		printf("Enter Your Choice : ");
+		scanf("%d", &userChoice);
+		
+		/* Validation */
+		if (userChoice < 1 || userChoice > 8) {
+			printf("Invlaid Input!\n");
+		} else {
+			/* Switch to call methods according userChoice */
+			switch(userChoice) {
+				case 1:
+//					squareArea();
+					break;
+				case 2:
+//					circleArea();
+					break;
+				case 3:
+//					rectangleArea();
+					break;
+				case 4:
+//					cubeArea();
+					break;
+				case 5:
+//					coneArea();
+					break;
+				case 6:
+//					sphereArea();
+					break;
+				case 7:
+//					cylinderArea();
+					break;
+				default:
+					return;
+			}
+		}
+	}
 }
