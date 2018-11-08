b4df0ca46a5bda32b8d51e81a7b64b5a1b8211e5
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-08 12:56:25 -0600

    Project Initialized

---
+++ parameterCalculator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+/* Global Variables */
+#define PI 3.14159
+
+/*
+ * Function 	: main
+ * Attributes 	: none
+ * Description  : Calls every method and run them
+ * Method Type	: void 
+ */
+int main() {
+	/* Code */
+	getch();
+	return 0;
+}
