2aff26c0b39800e155ecd523dbab0efd998ac790
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-08 13:33:17 -0600

    rectangleParameter is Added

--- parameterCalculator.c
+++ parameterCalculator.c
@@ -58,6 +58,34 @@ void circleParameter() {
 
 
 /*
+ * Function 	: rectangleParameter
+ * Attributes 	: none
+ * Variables	: sideOne	=>	Type : int		=> Store value of side 1 from user
+ *				  sideTwo	=>	Type : int		=> Store value of side 2 from user
+ *				  parameter	=>	Type : float	=> Store value of parameter
+ * Description  : take input and calculate parameter of circle
+ * Method Type	: void 
+ */
+void rectangleParameter() {
+	/* Variable */
+	int sideOne, sideTwo;
+	float parameter;
+	
+	printf("Enter Side One : ");
+	scanf("%d", &sideOne);
+	printf("Enter Side Two : ");
+	scanf("%d", &sideTwo);
+	
+	if (sideOne <= 0 || sideTwo <= 0) {
+		printf("Invalid Input!\n");
+		rectangleParameter();
+	} else {
+		parameter = 2 * sideOne * sideTwo;
+		printf("Parameter of your rectangle is : %.02f. \n", parameter);
+	}
+}
+
+/*
  * Function 	: main
  * Attributes 	: none
  * Description  : Calls every method and run them
@@ -90,7 +118,7 @@ void main() {
 					circleParameter();
 					break;
 				case 3:
-//					rectangleArea();
+					rectangleParameter();
 					break;
 				case 4:
 //					cubeArea();
